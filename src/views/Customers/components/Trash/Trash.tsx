0cdc3b278f30038b8019c06d4503f95353704054
VictorTamayoBriones <victortamayo509@gmail.com>
2023-06-20 20:26:46 -0600

    feat: :sparkles: update styles take out trash

--- src/views/Customers/components/Trash/Trash.tsx
+++ src/views/Customers/components/Trash/Trash.tsx
@@ -1,26 +1,32 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { CustomerContext } from '../../context/customers.context';
 import { ICustomer } from '@/models/customer.models';
-import { TrashContainer } from './trash.style';
+import { MainTrashContainer, TrashContainer } from './trash.style';
 function Trash() {
 
     const { customersDeletedList, takeOutTrashById } = useContext(CustomerContext)
+
+    const [trashIsOpen, setTrashIsOpen]=useState<boolean>(false);
+
   return (
-    <TrashContainer>
-    {
-        customersDeletedList.map((customer: ICustomer)=>{
-            return(
-                <div>
-                    <p>Name: {customer.full_name}</p>
-                    <p>NSS: {customer.nss}</p>
-                    <p>RFC: {customer.rfc}</p>
-                    <p>Phone: {customer.phone}</p>
-                    <button onClick={()=>takeOutTrashById(customer.id)} >take out</button>
-                </div>
-            )
-        })
-    }
-    </TrashContainer>
+    <MainTrashContainer>
+        <button className='showTrash' onClick={()=>setTrashIsOpen(!trashIsOpen)} >Show trash</button>
+        <TrashContainer status={trashIsOpen} >
+        {
+            customersDeletedList.map((customer: ICustomer)=>{
+                return(
+                    <div>
+                        <p>Name: {customer.full_name}</p>
+                        <p>NSS: {customer.nss}</p>
+                        <p>RFC: {customer.rfc}</p>
+                        <p>Phone: {customer.phone}</p>
+                        <button onClick={()=>takeOutTrashById(customer.id)} >take out</button>
+                    </div>
+                )
+            })
+        }
+        </TrashContainer>
+    </MainTrashContainer>
   )
 }
 
--- src/views/Customers/components/Trash/trash.style.tsx
+++ src/views/Customers/components/Trash/trash.style.tsx
@@ -1,22 +1,48 @@
 import { Theme } from '@/theme';
 import styled from 'styled-components';
 
-export const TrashContainer = styled.aside`
+interface Props{
+    status: boolean
+}
+
+export const MainTrashContainer = styled.div`
+    width: 250px;
+    position: absolute;
     position: absolute;
     top: 0;
     left: 0;
+    transition: all .3s ease-in-out;
+    .showTrash{
+        position: absolute;
+        top: 10px;
+        left: 20px;
+        border: none;
+        background: ${Theme.red};
+        color: #fff;
+        padding: 5px 10px;
+        border-radius: 3px;
+        cursor: pointer;
+    }
+`;
+
+export const TrashContainer = styled.aside<Props>`
+    width: ${(props)=>props.status ? '250px' : '0px'};
     background: #fff;
-    padding: 20px;
+    padding: 20px ${(props)=>props.status ? '20px' : '0px'};
     height: 100vh;
     overflow: scroll;
     border-right: solid 1px ${Theme.grisMedio};
+    transition: all .3s ease-in-out;
     div{
         margin: 20px 0;
-        background: ${Theme.grisMedio};
+        background: ${Theme.azul};
+        color: #fff;
+        text-align: justify;
         border-radius: 3px;
         padding: 10px;
         button{
             width: 100%;
+            cursor: pointer;
         }
     }
 `;
